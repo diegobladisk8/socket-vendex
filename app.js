d015768ae95a9ef2c7fa0b37c648dcefd179ca7a
Diego <diesk.20@gmail.com>
2022-03-23 02:07:08 -0500

    Cambios generales

--- app.js
+++ app.js
@@ -15,10 +15,12 @@ const server = http.createServer({
 }, app);
 
 const sio = require('socket.io')(server, {
+    allowEIO3: true,
     handlePreflightRequest: (req, res) => {
         const headers = {
             "Access-Control-Allow-Headers": "Content-Type, Authorization",
-            "Access-Control-Allow-Credentials": false
+            "Access-Control-Allow-Origin": "*", //req.headers.origin, or the specific origin you want to give access to,
+            "Access-Control-Allow-Credentials": true
         };
         res.writeHead(200, headers);
         res.end();
