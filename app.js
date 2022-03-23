50cc1521a1b6a408a0b6b3e95ac7f5121b8213ed
Diego <diesk.20@gmail.com>
2022-03-22 22:39:26 -0500

    Cambios generales

---
+++ app.js
@@ -0,0 +1,46 @@
+import { createRequire } from 'module';
+import createSubscriber from 'pg-listen';
+
+const require = createRequire(
+    import.meta.url);
+
+import express from "express";
+const http = require('http');
+const subscriber = createSubscriber({ connectionString: `postgres://${process.env.POSTGRESQL_USERNAME}:${process.env.POSTGRESQL_PASSWORD}@${process.env.POSTGRESQL_HOST}:5432/${process.env.POSTGRESQL_DATABASE}` })
+    //const subscriber = createSubscriber({ connectionString: 'postgres://postgres:postgres@localhost:5432/isacode' })
+const app = express();
+const server = http.createServer({
+    requestCert: false,
+    rejectUnauthorized: false
+}, app);
+
+const sio = require('socket.io')(server, {
+    handlePreflightRequest: (req, res) => {
+        const headers = {
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Credentials": false
+        };
+        res.writeHead(200, headers);
+        res.end();
+    }
+});
+
+subscriber.notifications.on("virtual", (payload) => {
+    try {
+
+        if (payload != null && payload != undefined) {
+            sio.emit(payload.channel, JSON.stringify({ mensaje: payload.mensaje, tipo: payload.tipo }));
+        }
+    } catch (error) {
+        console.log(error);
+    }
+
+})
+async function connect() {
+    await subscriber.connect()
+    await subscriber.listenTo("virtual")
+}
+
+server.listen(444);
+console.log('Server start port 444');
+connect();
