897e56bc79f3c474e745936ad95d9272a72655b7
Diego <diesk.20@gmail.com>
2022-03-23 02:14:31 -0500

    Cambios generales

--- app.js
+++ app.js
@@ -16,11 +16,14 @@ const server = http.createServer({
 
 const sio = require('socket.io')(server, {
     allowEIO3: true,
+    cors: {
+        origin: "https://pos.vendex.ec",
+        methods: ["GET", "POST"],
+    },
     handlePreflightRequest: (req, res) => {
         const headers = {
             "Access-Control-Allow-Headers": "Content-Type, Authorization",
             "Access-Control-Allow-Origin": "*", //req.headers.origin, or the specific origin you want to give access to,
-            "Access-Control-Allow-Credentials": true
         };
         res.writeHead(200, headers);
         res.end();
