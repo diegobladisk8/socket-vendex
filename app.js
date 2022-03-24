6ab49f77f7abe313d6547e301e45cbe46abe82e7
Diego <diesk.20@gmail.com>
2022-03-23 20:42:41 -0500

    Cambios generales

--- app.js
+++ app.js
@@ -6,17 +6,23 @@ import express from "express";
 
 //const http = require('http');
 
-const subscriber = createSubscriber({ connectionString: `postgres://${process.env.POSTGRESQL_USERNAME}:${process.env.POSTGRESQL_PASSWORD}@${process.env.POSTGRESQL_HOST}:5432/${process.env.POSTGRESQL_DATABASE}` })
+//const subscriber = createSubscriber({ connectionString: `postgres://${process.env.POSTGRESQL_USERNAME}:${process.env.POSTGRESQL_PASSWORD}@${process.env.POSTGRESQL_HOST}:5432/${process.env.POSTGRESQL_DATABASE}` })
 
-//const subscriber = createSubscriber({ connectionString: 'postgres://postgres:postgres@localhost:5432/isacode' })
+const subscriber = createSubscriber({ connectionString: 'postgres://postgres:postgres@localhost:5432/isacode' })
 
 const app = express();
 //const server = http.createServer(app);
+const server = require('http').Server(app);
 
-const http = require('http').Server(app);
-
-
-var io = require('socket.io')(http, { cors: { origin: '*' } });
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+        transports: ['websocket', 'polling'],
+        credentials: true
+    },
+    allowEIO3: true
+});
 
 subscriber.notifications.on("virtual", (payload) => {
     try {
@@ -33,6 +39,6 @@ async function connect() {
     await subscriber.listenTo("virtual")
 }
 
-http.listen(444);
+server.listen(444);
 console.log('Server start port 444');
 connect();
