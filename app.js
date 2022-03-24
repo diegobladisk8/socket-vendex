a3a1d95945c2e896cf80ad427e8e5ca8ab0ec70a
Diego <diesk.20@gmail.com>
2022-03-23 20:38:44 -0500

    Cambios generales

--- app.js
+++ app.js
@@ -4,32 +4,20 @@ const require = createRequire(
     import.meta.url);
 import express from "express";
 
-const http = require('http');
+//const http = require('http');
 
-const subscriber = createSubscriber({ connectionString: `postgres://${process.env.POSTGRESQL_USERNAME}:${process.env.POSTGRESQL_PASSWORD}@${process.env.POSTGRESQL_HOST}:5432/${process.env.POSTGRESQL_DATABASE}` })
+//const subscriber = createSubscriber({ connectionString: `postgres://${process.env.POSTGRESQL_USERNAME}:${process.env.POSTGRESQL_PASSWORD}@${process.env.POSTGRESQL_HOST}:5432/${process.env.POSTGRESQL_DATABASE}` })
 
-//const subscriber = createSubscriber({ connectionString: 'postgres://postgres:postgres@localhost:5432/isacode' })
+const subscriber = createSubscriber({ connectionString: 'postgres://postgres:postgres@localhost:5432/isacode' })
 
 const app = express();
-const server = http.createServer(app);
+//const server = http.createServer(app);
 
-const io = require('socket.io')(server, {
-    allowEIO3: true,
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"],
-        credentials: true
-    },
-    handlePreflightRequest: (req, res) => {
-        const headers = {
-            "Access-Control-Allow-Headers": "Content-Type, Authorization",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Credentials": true
-        };
-        res.writeHead(200, headers);
-        res.end();
-    }
-});
+const http = require('http').Server(app);
+
+
+var io = require('socket.io')(http, { cors: { origin: '*' } });
+
 subscriber.notifications.on("virtual", (payload) => {
     try {
         if (payload != null && payload != undefined) {
@@ -45,6 +33,6 @@ async function connect() {
     await subscriber.listenTo("virtual")
 }
 
-server.listen(444);
+http.listen(444);
 console.log('Server start port 444');
 connect();
