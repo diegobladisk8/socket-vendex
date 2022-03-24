7c4fd82f9b4ec05ba81d33271fc9efe867a79d89
Diego <diesk.20@gmail.com>
2022-03-23 20:51:58 -0500

    Cambios generales

--- app.js
+++ app.js
@@ -1,33 +1,41 @@
 import { createRequire } from 'module';
 import createSubscriber from 'pg-listen';
+
 const require = createRequire(
     import.meta.url);
-import express from "express";
 
-//const http = require('http');
+import express from "express";
+const https = require('https');
 
 const subscriber = createSubscriber({ connectionString: `postgres://${process.env.POSTGRESQL_USERNAME}:${process.env.POSTGRESQL_PASSWORD}@${process.env.POSTGRESQL_HOST}:5432/${process.env.POSTGRESQL_DATABASE}` })
 
 //const subscriber = createSubscriber({ connectionString: 'postgres://postgres:postgres@localhost:5432/isacode' })
 
 const app = express();
-//const server = http.createServer(app);
-const server = require('http').Server(app);
 
-const io = require('socket.io')(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"],
-        transports: ['websocket', 'polling'],
-        credentials: true
-    },
-    allowEIO3: true
+const server = https.createServer({
+    requestCert: false,
+    rejectUnauthorized: false
+}, app);
+
+const sio = require('socket.io')(server, {
+    handlePreflightRequest: (req, res) => {
+        const headers = {
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true
+        };
+        res.writeHead(200, headers);
+        res.end();
+    }
 });
+
 
 subscriber.notifications.on("virtual", (payload) => {
     try {
+
         if (payload != null && payload != undefined) {
-            io.emit(payload.channel, JSON.stringify({ mensaje: payload.mensaje, tipo: payload.tipo }));
+            sio.emit(payload.channel, JSON.stringify({ mensaje: payload.mensaje, tipo: payload.tipo }));
         }
     } catch (error) {
         console.log(error);
@@ -38,6 +46,7 @@ async function connect() {
     await subscriber.connect()
     await subscriber.listenTo("virtual")
 }
+
 
 server.listen(444);
 console.log('Server start port 444');
